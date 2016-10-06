1fd00ec045406ab8789e68cf5d338e2b7e37cb06
sergiox93 <sergio.lancheros@gmail.com>
2016-10-06 08:22:17 -0500

    login
    proyecto de login

---
+++ login/config/routes.js
@@ -0,0 +1,20 @@
+module.exports.routes = {
+  '/': {
+    view: 'homepage'
+  },
+
+  'get /login': {
+       view: 'login'
+  },
+
+  'post /login': 'AuthController.login',
+
+  '/logout': 'AuthController.logout',
+
+  'get /signup': {
+    view: 'signup'
+  },
+
+    'get /bye': { view: 'bye' }
+
+};
